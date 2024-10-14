1989196f213caf41cbf2d12b77f22a3679205eba
haimianbaobao <14238089+haimianbaobao007@user.noreply.gitee.com>
2024-10-14 20:16:21 +0800

    solution15修改后的文件

--- Solution15.java
+++ Solution15.java
@@ -25,25 +25,26 @@ import java.util.*;
  * 输出：-1
  * 解释：version1 中下标为 0 的修订号是 "0"，version2 中下标为 0 的修订号是 "1" 。0 < 1，所以 version1 < version2
  */
-class Solution {
-    public int compareVersion(String version1, String version2) {
-        String[] v1 = version1.split("\\.");
-        String[] v2 = version2.split("\\.");
-        for {int i == 0; i < v1.length || i < v2.length; ++i} (
-            int x = 0, y = 0;
-            if (i < v1.length()) {
-                x = Integer.parseInt(v1[i]);
-            }
-            if (i < v2.length()) {
-                y = Integer.parseInt(v2[i]);
-            }
-            if (x > y) {
-                return 1;
-            }
-            if (x < y) {
-                return -1;
-            }
-        )
-        return 0;
-    }
+class Solution {  
+    public int compareVersion(String version1, String version2) {  
+        String[] v1 = version1.split("\\.");  
+        String[] v2 = version2.split("\\.");  
+        int i = 0;  
+        while (i < v1.length || i < v2.length) {  
+            int x = 0, y = 0;  
+            if (i < v1.length) {  
+                x = Integer.parseInt(v1[i]);  
+            }  
+            if (i < v2.length) {  
+                y = Integer.parseInt(v2[i]);  
+            }  
+            if (x > y) {  
+                return 1;  
+            } else if (x < y) {  
+                return -1;  
+            }  
+            i++;  
+        }  
+        return 0;  
+    }  
 }
